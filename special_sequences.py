68fd24344f4f70ff79da3549eb1895daf2e07a1d
Surya <suryapratapsingh0833@gmail.com>
2024-04-15 12:25:45 +0530

    new commit

--- special_sequences.py
+++ special_sequences.py
@@ -3,3 +3,7 @@ a="harry potter"
 print("1. \A symbol matches if staring of sting contain that value")
 match=re.search(r"\Ahar",a)
 print(match)
+print("1. \b symbol matches if staring or ending of sting contain that value")
+match=re.search(r"\bhar",a)
+match1=re.search(r"er\b",a)
+print(match,match1)
